a80fdc95605c882c3900d8a742ee8d274baea25b
jlennomatias <john.x12@hotmail.com>
2022-09-13 19:40:32 -0300

    first commit

---
+++ passagens/views.py
@@ -0,0 +1,9 @@
+from ast import Pass
+from django.shortcuts import render
+from passagens.forms import PassagemForms
+
+# Create your views here.
+def index(request):
+    form = PassagemForms()
+    contexto = {'form':form}
+    return render(request, 'index.html', contexto)
